24b46441251207443cf8e555bd0665a61cc58f9c
erichon <erichon18@gmail.com>
2022-03-31 00:51:47 +0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,20 @@
+radio.setGroup(1)
+basic.forever(function () {
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . # . .
+        . . . . .
+        . . . . .
+        `)
+    radio.sendNumber(1)
+    basic.pause(500)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.pause(500)
+})
